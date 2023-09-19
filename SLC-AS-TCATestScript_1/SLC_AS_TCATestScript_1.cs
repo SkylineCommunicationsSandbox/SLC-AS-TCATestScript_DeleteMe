b560cacedf345d556e0eda0f7ebaab122ed4f452
Toon Casteele <toon.casteele@skyline.be>
2023-09-19 16:31:26 +0200

    Add order 1

--- SLC-AS-TCATestScript_1/SLC_AS_TCATestScript_1.cs
+++ SLC-AS-TCATestScript_1/SLC_AS_TCATestScript_1.cs
@@ -55,8 +55,9 @@ namespace SLC_AS_TCATestScript_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room0;
 	using Skyline.DataMiner.Automation;
-	
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -69,6 +70,8 @@ namespace SLC_AS_TCATestScript_1
 		public void Run(IEngine engine)
 		{
 			engine.GenerateInformation("Hello world");
+			var order = OrderFactory.CreateOrder("Order 1");
+			order.Dispatch("Toon Casteele");
 		}
 	}
 }
